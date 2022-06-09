8744a772425cdeb64cfdea28e3740c546a66d343
TimiMaliki <malikitimi@gmail.com>
2022-06-09 04:43:14 +0100

    js commit

---
+++ assets/src/css/js/main.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalButtons = document.querySelector('data-operation');
+const deleteButtons = document.querySelector('data-delete');
+const allClearButtonButtons = document.querySelector('data-all-clear');
+const previousOperanTextElement = document.querySelector('data-previous-operand');
+const currentOperanTextElement = document.querySelector('data-current-operand');
